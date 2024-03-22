a027554f4668f617a43b6e96c03b2c25193683ae
Chris Peters <stardust1689@gmail.com>
2024-03-22 00:50:40 -0800

    Setup of Express.js server with packages

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
 
+const app = express();
+const port = 3400;
 
-console.log("hello world");
+app.use(morgan("combined"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/status", (req, res) => {
+	res.send({
+		message: "Hello World"
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
